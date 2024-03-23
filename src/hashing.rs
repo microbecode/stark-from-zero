1c4cac086e7459894ae3c4455e63d794d73fe951
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2024-03-23 21:48:19 +0200

    Fix hashing

--- src/hashing.rs
+++ src/hashing.rs
@@ -1,8 +1,8 @@
-pub fn hash(input: &str) -> i64 {
-    let mut hash: i64 = 0;
+pub fn hash(input: &str) -> u64 {
+    let mut hash: u64 = 0;
     for c in input.chars() {
-        let char_num = c as i64;
-        hash = hash.wrapping_mul(1000); // Shift
+        let char_num = c as u64;
+        hash = hash.wrapping_mul(113); // Shift by a prime number so char positions make a difference
         hash = hash.wrapping_add(char_num);
     }
     hash
@@ -18,6 +18,9 @@ mod tests {
         assert!(hash("world") > 0);
         assert_eq!(hash("hello"), hash("hello"));
         assert_ne!(hash("hello"), hash("world"));
+
+        assert_ne!(hash("hello"), hash("ehllo"));
+        assert_ne!(hash("hello"), hash("olleh"));
 
         assert_ne!(hash("ab"), hash("ba"));
         assert_ne!(hash("ab "), hash("ab"));
