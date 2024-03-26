2b659acfafe1c3f49cb016924c49a2134cae0cab
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2024-03-26 19:31:37 +0200

    fix for odd leaves: duplicate

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -16,18 +16,17 @@ impl MerkleTree {
 
     fn build(&mut self, elements: &[FiniteFieldElement]) {
         let mut hashes: Vec<u64> = elements.iter().map(|e| e.hash()).collect();
+        if hashes.len() % 2 != 0 {
+            // If odd number, duplicate the last element
+            hashes.push(hashes[hashes.len() - 1]);
+        }
         let mut nodes = Vec::new();
         nodes.push(hashes.clone());
 
         while hashes.len() > 1 {
             let mut new_hashes = Vec::new();
             for chunk in hashes.chunks(2) {
-                let (left, right) = match chunk.len() {
-                    1 => (chunk[0], chunk[0]),
-                    2 => (chunk[0], chunk[1]),
-                    _ => unreachable!(),
-                };
-                let hash = hashing::hash(left.wrapping_add(right));
+                let hash = hashing::hash(chunk[0].wrapping_add(chunk[1]));
 
                 new_hashes.push(hash); // ab cd ef
             }
@@ -54,7 +53,6 @@ impl MerkleTree {
                 break; // Reached the root node, no need to continue
             }
             let sibling_idx = if idx % 2 == 0 { idx + 1 } else { idx - 1 };
-            println!("level: {} {}", level.len(), sibling_idx);
             proof.push(level[sibling_idx]);
             idx /= 2;
         }
@@ -98,13 +96,15 @@ mod tests {
         elements.push(FiniteFieldElement::new(val, field));
         tree.build(&elements);
 
-        let expected = hash(val);
+        let expected_leaf = hash(val);
+        let expected_root = hash(expected_leaf.wrapping_add(expected_leaf));
 
-        assert_eq!(tree.levels.len(), 1);
-        assert_eq!(tree.levels[0].len(), 1);
+        assert_eq!(tree.levels.len(), 2);
+        assert_eq!(tree.levels[0].len(), 2);
 
-        assert_eq!(tree.root, Some(expected));
-        assert_eq!(tree.levels[0][0], expected);
+        assert_eq!(tree.root, Some(expected_root));
+        assert_eq!(tree.levels[0][0], expected_leaf);
+        assert_eq!(tree.levels[0][1], expected_leaf);
     }
 
     #[test]
@@ -151,14 +151,15 @@ mod tests {
         let expected_leaf_1 = hash(val1);
         let expected_leaf_2 = hash(val2);
         let expected_leaf_3 = hash(val3);
+        let expected_leaf_4 = hash(val3);
 
         let expected_mid_node1 = hash(expected_leaf_1.wrapping_add(expected_leaf_2));
-        let expected_mid_node2 = hash(expected_leaf_3.wrapping_add(expected_leaf_3));
+        let expected_mid_node2 = hash(expected_leaf_3.wrapping_add(expected_leaf_4));
 
         let expected_root = hash(expected_mid_node1.wrapping_add(expected_mid_node2));
 
         assert_eq!(tree.levels.len(), 3);
-        assert_eq!(tree.levels[0].len(), 3);
+        assert_eq!(tree.levels[0].len(), 4);
         assert_eq!(tree.levels[1].len(), 2);
         assert_eq!(tree.levels[2].len(), 1);
 
@@ -168,6 +169,7 @@ mod tests {
         assert_eq!(tree.levels[0][0], expected_leaf_1);
         assert_eq!(tree.levels[0][1], expected_leaf_2);
         assert_eq!(tree.levels[0][2], expected_leaf_3);
+        assert_eq!(tree.levels[0][3], expected_leaf_4);
 
         assert_eq!(tree.levels[1][0], expected_mid_node1);
         assert_eq!(tree.levels[1][1], expected_mid_node2);
@@ -191,23 +193,53 @@ mod tests {
         let expected_leaf_1 = hash(val1);
         let expected_leaf_2 = hash(val2);
         let expected_leaf_3 = hash(val3);
+        let expected_leaf_4 = hash(val3);
 
         let expected_mid_node1 = hash(expected_leaf_1.wrapping_add(expected_leaf_2));
-        let expected_mid_node2 = hash(expected_leaf_3.wrapping_add(expected_leaf_3));
+        let expected_mid_node2 = hash(expected_leaf_3.wrapping_add(expected_leaf_4));
 
         let expected_root = hash(expected_mid_node1.wrapping_add(expected_mid_node2));
 
-        let proof = tree.get_merkle_proof(0).unwrap();
-        let expected_proof = vec![expected_leaf_2, expected_mid_node2, expected_root];
+        // Test proofs for each leaf
+        {
+            let proof = tree.get_merkle_proof(0).unwrap();
+            let expected_proof = vec![expected_leaf_2, expected_mid_node2, expected_root];
 
-        for a in proof.iter() {
-            println!("Proof {}", a);
+            assert_eq!(proof.len(), expected_proof.len());
+
+            for (elem1, elem2) in proof.iter().zip(expected_proof.iter()) {
+                assert_eq!(elem1, elem2); // Ensure each pair of corresponding elements is equal
+            }
         }
+        {
+            let proof = tree.get_merkle_proof(1).unwrap();
+            let expected_proof = vec![expected_leaf_1, expected_mid_node2, expected_root];
 
-        assert_eq!(proof.len(), expected_proof.len());
+            assert_eq!(proof.len(), expected_proof.len());
 
-        for (elem1, elem2) in proof.iter().zip(expected_proof.iter()) {
-            assert_eq!(elem1, elem2); // Ensure each pair of corresponding elements is equal
+            for (elem1, elem2) in proof.iter().zip(expected_proof.iter()) {
+                assert_eq!(elem1, elem2); // Ensure each pair of corresponding elements is equal
+            }
+        }
+        {
+            let proof = tree.get_merkle_proof(2).unwrap();
+            let expected_proof = vec![expected_leaf_4, expected_mid_node1, expected_root];
+
+            assert_eq!(proof.len(), expected_proof.len());
+
+            for (elem1, elem2) in proof.iter().zip(expected_proof.iter()) {
+                assert_eq!(elem1, elem2); // Ensure each pair of corresponding elements is equal
+            }
+        }
+        {
+            let proof = tree.get_merkle_proof(3).unwrap();
+            let expected_proof = vec![expected_leaf_3, expected_mid_node1, expected_root];
+
+            assert_eq!(proof.len(), expected_proof.len());
+
+            for (elem1, elem2) in proof.iter().zip(expected_proof.iter()) {
+                assert_eq!(elem1, elem2); // Ensure each pair of corresponding elements is equal
+            }
         }
     }
 }
